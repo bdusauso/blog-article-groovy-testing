58eb1bdfdd82f16d75d34e99c3095390aade3c2a
Bruno Dusausoy <bdusausoy@yp5.be>
2013-06-13 16:33:04 +0200

    Added dataset loading.

--- src/test/groovy/be/codinsanity/sandbox/repository/MeetingRepositoryTest.groovy
+++ src/test/groovy/be/codinsanity/sandbox/repository/MeetingRepositoryTest.groovy
@@ -1,5 +1,10 @@
 package be.codinsanity.sandbox.repository
 
+import org.dbunit.DataSourceDatabaseTester
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder
+import org.dbunit.operation.DatabaseOperation
+import org.junit.Before
+import org.springframework.core.io.ClassPathResource
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
 import spock.lang.Specification
@@ -23,5 +28,15 @@ class MeetingRepositoryTest extends Specification {
     def "Findall should return "() {
         expect:
         assert meetingRepository.findAll() != null
+    }
+
+    @Before
+    def setup() {
+        def databaseTest = new DataSourceDatabaseTester(dataSource)
+        def resource = new ClassPathResource("be/codinsanity/sandbox/repository/meeting/search.xml")
+        def dataset = new FlatXmlDataSetBuilder().build(resource.getInputStream());
+        databaseTest.setDataSet(dataset)
+        def dbConnection = databaseTest.getConnection()
+        DatabaseOperation.CLEAN_INSERT.execute(dbConnection, dataset)
     }
 }
