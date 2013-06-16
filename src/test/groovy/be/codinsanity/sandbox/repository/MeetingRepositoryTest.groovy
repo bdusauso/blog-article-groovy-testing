ce15f3095b797534bc05546e018ac44a3a2c2046
Bruno Dusausoy <bdusausoy@yp5.be>
2013-06-16 13:47:37 +0200

    Indentation, always the indentation.

--- src/test/groovy/be/codinsanity/sandbox/repository/MeetingRepositoryTest.groovy
+++ src/test/groovy/be/codinsanity/sandbox/repository/MeetingRepositoryTest.groovy
@@ -3,11 +3,11 @@ package be.codinsanity.sandbox.repository
 import org.dbunit.DataSourceDatabaseTester
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder
 import org.dbunit.operation.DatabaseOperation
-import org.junit.Before
 import org.springframework.core.io.ClassPathResource
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.transaction.annotation.Transactional
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import javax.annotation.Resource
 import javax.sql.DataSource
@@ -25,16 +25,19 @@ class MeetingRepositoryTest extends Specification {
     @Resource
     DataSource dataSource
 
-    def "FindOne should return an existing entity"() {
+    @Unroll
+    def "findOne(#id) should return an existing entity"() {
+        setup:
+            init()
+
         expect:
-        meetingRepository.findOne(id) != null
+            meetingRepository.findOne(id) != null
 
         where:
-        id << [1L, 2L, 3L]
+            id << [1L, 2L, 3L, 4L]
     }
 
-    @Before
-    def setup() {
+    def init() {
         def databaseTest = new DataSourceDatabaseTester(dataSource)
         def resource = new ClassPathResource("be/codinsanity/sandbox/repository/meeting/search.xml")
         def dataset = new FlatXmlDataSetBuilder().build(resource.getInputStream());
